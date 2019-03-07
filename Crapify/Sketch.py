317841dd5a47c0b3480074ab29da5362e1dce691
Héctor Mauricio de la Rosa Prado <holyfiddlex@gmail.com>
2019-03-07 12:54:19 -0600

    Crapify setup

---
+++ Crapify/Sketch.py
@@ -0,0 +1,21 @@
+import imageio
+import numpy as np
+import scipy.ndimage
+import matplotlib.pyplot as plt
+
+def grayscale(rgb):
+  return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+def dodge(front,back):
+  result=front*255/(255-back) 
+  result[np.logical_or(result > 255, back ==255)] =255
+  return result.astype('uint8')
+
+start_img = imageio.imread("./dog.jpg")
+gray_img = grayscale(start_img)
+gray_inv_img = 255-gray_img
+blur_img = scipy.ndimage.filters.gaussian_filter(gray_inv_img,sigma=7)
+final_img= dodge(blur_img,gray_img) 
+plt.imshow(final_img, cmap="gray")
+plt.show()
+plt.imsave('sketch.png', final_img, cmap='gray', vmin=0, vmax=255)
